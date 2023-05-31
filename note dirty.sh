8fb5c4ef71eea8f5ce7884dd511f224617ed6c26
Hung-TSANG-TUNG <72122216+Hung-TSANG-TUNG@users.noreply.github.com>
2023-05-31 16:43:08 +0400

    Update note dirty.sh

--- note dirty.sh
+++ note dirty.sh
@@ -1,25 +1,26 @@
-Le script tente de mettre en place une protection contre le Dirty COW. Voici une explication du script :
 
-1. Il définit certaines variables, telles que la variable SHELL pour spécifier le chemin vers le shell, la variable TFILE pour spécifier un fichier temporaire, 
-et la variable FNAME pour spécifier un nom de fichier temporaire.
+1. Définition des variables : Le script commence par définir certaines variables,
+telles que le chemin vers le shell (`SHELL`), le fichier de socket X11 (`TFILE`), et le nom du fichier temporaire (`FNAME`).
 
-2. Il définit une fonction sigsegv_handler() qui gère les signaux SIGSEGV (segmentation fault).
+2. Gestion du signal SIGSEGV : Le script définit une fonction (`sigsegv_handler()`) qui sera appelée en cas de réception du signal SIGSEGV (segmentation fault).
 
-3. Il définit une fonction spawn_shell() qui exécute le shell spécifié dans la variable SHELL.
+3. Lancement d'un shell : Une fonction (`spawn_shell()`) est définie pour exécuter le shell spécifié dans la variable `SHELL`.
 
-4. Il définit une fonction race() qui exécute une boucle tant que la variable g_done est fausse. Dans la boucle, il vérifie si le fichier spécifié par la variable TFILE existe. 
-S'il existe, il crée un fichier temporaire avec la commande mktemp, écrit "xyzzy" dedans, puis le supprime.
-Ensuite, il utilise la commande madvise pour modifier les attributs de la mémoire.
+4. Course conditionnelle (race condition) : Une fonction (`race()`) est définie pour créer une boucle tant que la variable `g_done` est fausse.
+À chaque itération de la boucle, le script vérifie si le fichier spécifié par `TFILE` existe. Si c'est le cas, il crée un fichier temporaire, y écrit "xyzzy", puis le supprime.
+Ensuite, il utilise la commande `madvise` pour modifier les attributs de la mémoire.
 
-5. Il définit une fonction do_race() qui prend une adresse en argument. Dans cette fonction, il exécute la fonction race() plusieurs fois en parallèle à l'aide de la boucle for. 
-Il stocke les identifiants de processus (PID) des processus en cours d'exécution dans le tableau pids. 
-Ensuite, il attend pendant 2 secondes, exécute spawn_shell() pour ouvrir un shell et met à jour la variable g_done pour arrêter les boucles. 
-Enfin, il attend que tous les processus se terminent avec la commande wait.
+5. Exploitation de la race condition : Une autre fonction (`do_race()`) est définie pour exécuter plusieurs instances de la fonction `race()` en parallèle.
+Ces instances sont lancées dans une boucle et les identifiants de processus (PID) sont stockés dans un tableau (`pids`). 
+Après un délai de 2 secondes, le script exécute `spawn_shell()` pour ouvrir un nouveau shell, met à jour la variable `g_done` pour arrêter les boucles 
+et attend la fin de tous les processus en utilisant la commande `wait`.
 
-6. Il utilise la commande mktemp pour créer un fichier temporaire et la commande sudo mmap pour effectuer une allocation de mémoire et mapper le fichier temporaire en mémoire. 
-Si l'allocation et le mappage réussissent, il exécute la commande sudo mmap pour modifier les attributs de la mémoire.
+6. Protection contre Dirty COW : Le script utilise la commande `mktemp` pour créer un fichier temporaire et la commande `sudo mmap`
+pour effectuer une allocation de mémoire et mapper le fichier temporaire en mémoire.
+S'il n'y a pas d'échec, le script utilise à nouveau `sudo mmap` pour modifier les attributs de la mémoire.
 
-7. Il appelle la fonction do_race() avec l'adresse mappée en argument.
+7. Exécution de la course conditionnelle : Le script appelle la fonction `do_race()` avec l'adresse mappée en argument.
 
-En résumé, le script tente de créer une race condition en utilisant les commandes mmap et madvise pour tenter de se protéger contre l'exploitation de la vulnérabilité Dirty COW. 
-Cependant, la fiabilité de cette approche n'est pas garantie et il est recommandé de se fier aux correctifs officiels fournis par les distributions Linux pour se proteger.
+En résumé, le script tente de créer une race condition en utilisant les commandes `mmap` et `madvise` pour potentiellement se protéger du Dirty COW.
+Cependant, il est important de noter que cette méthode n'est pas une solution de protection complète et fiable.
+Pour une protection adéquate, il est recommandé de maintenir votre système à jour en appliquant les correctifs de sécurité fournis par votre distribution Linux.
