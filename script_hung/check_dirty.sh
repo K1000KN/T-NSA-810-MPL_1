717686c2f5788b50198becb0381042381f243b5d
Hung-TSANG-TUNG <72122216+Hung-TSANG-TUNG@users.noreply.github.com>
2023-06-02 15:43:59 +0400

    Update check_dirty.sh

--- script_hung/check_dirty.sh
+++ script_hung/check_dirty.sh
@@ -5,11 +5,11 @@ kernel_version=$(uname -r)
 echo "Version du noyau : $kernel_version"
 
 # Vérification de la date du noyau
-kernel_date=$(cat /proc/version | awk '{print $3}')
+kernel_date=$(cat /proc/version | awk '{print $11, $7, $8}')
 echo "Date du noyau : $kernel_date"
 
 # Vérification de la vulnérabilité Dirty COW
-if [[ "$kernel_date" < "2016-10-20" ]]; then
+if [[ "$kernel_date" < "2016 Oct 20" ]]; then
   echo "Votre système est potentiellement vulnérable à Dirty COW."
 else
   echo "Votre système n'est pas vulnérable à Dirty COW."
